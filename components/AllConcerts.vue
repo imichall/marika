e3d0698cd958df0ca237939cd35436058898caff
iMichall <iimichal.svoboda@gmail.com>
2024-12-21 18:49:30 +0100

    Add breadcrumbs to all concers

--- components/AllConcerts.vue
+++ components/AllConcerts.vue
@@ -1,5 +1,48 @@
 <template>
   <section class="pb-16 mt-[100px]">
+    <div class="container mx-auto px-4 mb-8">
+      <nav class="flex" aria-label="Breadcrumb">
+        <ol class="inline-flex items-center space-x-1 md:space-x-3">
+          <li class="items-center">
+            <NuxtLink
+              to="/"
+              class="text-gray-700 hover:text-red-800 flex items-center"
+            >
+              <svg
+                class="w-4 h-4 mr-2"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"
+                />
+              </svg>
+              Domů
+            </NuxtLink>
+          </li>
+          <li>
+            <div class="flex items-center">
+              <svg
+                class="w-6 h-6 text-gray-400"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+              <span class="ml-1 text-gray-500 md:ml-2">
+                {{ concert?.title || "Všechny koncerty" }}
+              </span>
+            </div>
+          </li>
+        </ol>
+      </nav>
+    </div>
     <div class="container mx-auto px-4">
       <h1 class="text-3xl font-bold mb-8 text-center">Všechny koncerty</h1>
       <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
