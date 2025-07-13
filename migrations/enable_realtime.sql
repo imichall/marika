ca1ff80c79f727a0c1c7e3ca571dce63f65a8a86
imichall <iimichal.svoboda@gmail.com>
2025-07-13 17:29:12 +0200

    Delete migrations directory

--- migrations/enable_realtime.sql
+++
@@ -1,5 +0,0 @@
--- Enable realtime for ticket_orders table
-ALTER PUBLICATION supabase_realtime ADD TABLE ticket_orders;
-
--- Enable realtime for specific columns
-ALTER TABLE ticket_orders REPLICA IDENTITY FULL;
